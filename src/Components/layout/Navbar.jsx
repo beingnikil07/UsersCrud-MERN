29c32f0702a0e0ad47b97549ef2d69a1ab645ab2
Nikil kumar <kumarnikil220@gmail.com>
2023-01-09 15:13:04 +0530

    Navbar created successfully

--- src/Components/layout/Navbar.jsx
+++ src/Components/layout/Navbar.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import { AppBar,Toolbar,styled} from '@mui/material';
+import { AppBar, Toolbar, styled } from '@mui/material';
+const Header = styled(AppBar)`     // AppBar ko styled kiya hai to hum isko avv svv jagah Header ke naam se janenge.
+    background:#111111;
+`;
+const Tabs=styled('p')`
+     font-size:20px;
+     margin-right:20px;
+    
+`;
 
-const Navbar=()=>{
-    return(
+const Navbar = () => {
+    return (
         <div>
-           <AppBar>
-              <Toolbar>
-
-              </Toolbar>
-           </AppBar>           
+            <Header position='static'>
+                <Toolbar>
+                  <Tabs>Users</Tabs>
+                  <Tabs>Home</Tabs>
+                  <Tabs>Add User</Tabs>                  
+                </Toolbar>
+            </Header>
         </div>
     )
 }
