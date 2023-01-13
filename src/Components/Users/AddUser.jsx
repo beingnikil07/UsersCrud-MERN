168e74e7778ba63620552a331f903a7c92cd9d78
Nikil kumar <kumarnikil220@gmail.com>
2023-01-13 10:52:12 +0530

    navigated home page after add a user

--- src/Components/Users/AddUser.jsx
+++ src/Components/Users/AddUser.jsx
@@ -1,75 +1,107 @@
-import { FormControl, FormGroup, Input, InputLabel, Typography, styled, Button } from '@mui/material';
-import { React, useState } from 'react'
-import { addUser } from '../Services/api';
+import {
+  FormControl,
+  FormGroup,
+  Input,
+  InputLabel,
+  Typography,
+  styled,
+  Button,
+} from "@mui/material";
+import { React, useState } from "react";
+import { addUser } from "../Services/api";
+import { useNavigate } from "react-router";
 const Container = styled(FormGroup)`
-    width:50%;
-    font-family: 'Poppins', sans-serif;
-    margin:auto;
-    &>div{
-        margin-top:25px;
-    }
-    &>h4{
-        font-family: 'Poppins', sans-serif;
-        font-weight:600;
-        text-align:center;
-        margin-top:10px
-    }
-    &>button{
-        margin-top:30px;
-        border-radius:5px;
-    }
-    &>button:hover{
-        background-color:green;
-    }
+  width: 50%;
+  font-family: "Poppins", sans-serif;
+  margin: auto;
+  & > div {
+    margin-top: 25px;
+  }
+  & > h4 {
+    font-family: "Poppins", sans-serif;
+    font-weight: 600;
+    text-align: center;
+    margin-top: 10px;
+  }
+  & > button {
+    margin-top: 30px;
+    border-radius: 5px;
+  }
+  & > button:hover {
+    background-color: green;
+  }
 `;
 
 const AddUser = () => {
-    // default values of user 
-    const defaultUser = {
-        name: "",
-        username: "",
-        email: "",
-        phone: ""
-    }
-    const [user, setUser] = useState(defaultUser);
-    const onValueChange = (e) => {
-        //console.log(e.target.name,e.target.value);    
-        // setUser({...user,e.target.name:e.target.value}); Don't write like this becz we all know that object
-        //is a pair of key and value .Whenever key and value are same we have write key in a square bracket 
-        setUser({ ...user, [e.target.name]: e.target.value })
-        //spread ...user isliye kiya agar nhi lagate to ye override krr deta, if you wanna try what happens when we not gives spread operator
-        // just remove ...user and print console.log(user) to show the effect on console live
-        //console.log(user);   // yes ,working perfectly
-    }
+  // default values of user
+  const defaultUser = {
+    name: "",
+    username: "",
+    email: "",
+    phone: "",
+  };
+  const [user, setUser] = useState(defaultUser);
+  const navigate = useNavigate();
 
-    //Isme hum api api call karayenge mean jaise he user button prr click karega to api hit hogi ek 
-    //Avv api jo hai wo async hai to aapko iss function ko bhi async banana padega 
-    const addUserDetails = async () => {
-        await addUser(user);   //passing user as data to api and api accept it as data
-    }
-    return (
-        <Container>
-            <Typography variant='h4'>Add User</Typography>
-            <FormControl>
-                <InputLabel>Name</InputLabel>
-                <Input onChange={(e) => { onValueChange(e) }} name="name" />
-            </FormControl>
-            <FormControl>
-                <InputLabel>Username</InputLabel>
-                <Input onChange={(e) => { onValueChange(e) }} name="username" />
-            </FormControl>
-            <FormControl>
-                <InputLabel>Email</InputLabel>
-                <Input onChange={(e) => { onValueChange(e) }} name="email" />
-            </FormControl>
-            <FormControl>
-                <InputLabel>Phone</InputLabel>
-                <Input onChange={(e) => { onValueChange(e) }} name="phone" />
-            </FormControl>
-            <Button color='secondary' variant="contained" onClick={addUserDetails}>Add User</Button>
-        </Container>
-    )
-}
+  const onValueChange = (e) => {
+    //console.log(e.target.name,e.target.value);
+    // setUser({...user,e.target.name:e.target.value}); Don't write like this becz we all know that object
+    //is a pair of key and value .Whenever key and value are same we have write key in a square bracket
+    setUser({ ...user, [e.target.name]: e.target.value });
+    //spread ...user isliye kiya agar nhi lagate to ye override krr deta, if you wanna try what happens when we not gives spread operator
+    // just remove ...user and print console.log(user) to show the effect on console live
+    //console.log(user);   // yes ,working perfectly
+  };
+
+  //Isme hum api api call karayenge mean jaise he user button prr click karega to api hit hogi ek
+  //Avv api jo hai wo async hai to aapko iss function ko bhi async banana padega
+  const addUserDetails = async () => {
+    await addUser(user); //passing user as data to api and api accept it as data
+    navigate("/");
+  };
+  return (
+    <Container>
+      <Typography variant="h4">Add User</Typography>
+      <FormControl>
+        <InputLabel>Name</InputLabel>
+        <Input
+          onChange={(e) => {
+            onValueChange(e);
+          }}
+          name="name"
+        />
+      </FormControl>
+      <FormControl>
+        <InputLabel>Username</InputLabel>
+        <Input
+          onChange={(e) => {
+            onValueChange(e);
+          }}
+          name="username"
+        />
+      </FormControl>
+      <FormControl>
+        <InputLabel>Email</InputLabel>
+        <Input
+          onChange={(e) => {
+            onValueChange(e);
+          }}
+          name="email"
+        />
+      </FormControl>
+      <FormControl>
+        <InputLabel>Phone</InputLabel>
+        <Input
+          onChange={(e) => {
+            onValueChange(e);
+          }}
+          name="phone"
+        />
+      </FormControl>
+      <Button color="secondary" variant="contained" onClick={addUserDetails}>
+        Add User
+      </Button>
+    </Container>
+  );
+};
 export default AddUser;
-
-
