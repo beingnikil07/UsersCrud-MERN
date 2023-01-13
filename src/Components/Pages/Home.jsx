edfbf6e95d33d4e04a2c1176e1b9470df04c42ef
Nikil kumar <kumarnikil220@gmail.com>
2023-01-13 09:27:14 +0530

    Table created with Material UI

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,9 +1,21 @@
+import { Table,TableBody,TableCell, TableHead, TableRow } from '@mui/material';
 import React from 'react'
 const Home=()=>{
     return(
-        <div>
-            <h1>Home</h1>
-        </div>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Id</TableCell>
+                    <TableCell>Name</TableCell>
+                    <TableCell>User Name</TableCell>
+                    <TableCell>Email</TableCell>
+                    <TableCell>Phone</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                
+            </TableBody>
+        </Table>
     )
 }
 export default Home;
